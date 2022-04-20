a075e2be8af1ef8aa3d7d2bf389cb92eb547578c
Bossman <ghowusuboss@gmail.com>
2022-04-20 00:04:05 +0000

    About Me Page setup

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-// alert('This site of Bossman is under construction.');
-
-
-//  Wear your helmet and be cautious not to step on a nail
-//  Glacias!!');
-// alert('This site of Bossman is under construction');
+alert('This site of Bossman is under construction. Wear your helmet and be cautious not to step on a nail. His Spratagiousness!!')
